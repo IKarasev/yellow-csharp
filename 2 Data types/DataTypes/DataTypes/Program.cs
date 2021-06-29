7355f4fbff4882f43838a73d8ca9e23843f0fc93
Ilya Karasev <iakarsev@gmail.com>
2021-06-29 18:32:56 +0300

    Complited data types and conditions. Skip loops - don in ms.

--- 2 Data types/DataTypes/DataTypes/Program.cs
+++ 2 Data types/DataTypes/DataTypes/Program.cs
@@ -28,6 +28,43 @@ namespace DataTypes
             Console.WriteLine("\n__Text types:");
             Console.WriteLine($"char (16bits)   [{char.MinValue} ; {char.MaxValue}]");
 
+            // Changing the Type of Data
+            Console.WriteLine("\n__Changing the Type of Data");
+            //      Widening and Narrowing 
+            Console.WriteLine("Widening:");
+            //      changin the type from "smaller" to "bigger"
+            int i = 25;
+            float f = i;
+            Console.WriteLine($"{i.GetType()} i = {i}");
+            Console.WriteLine($"{f.GetType()} f = i = {f}");
+
+            Console.WriteLine("\nNarrowing:");
+            //       narrowing needs explicit expression
+            double d = 1.5;
+            // f = d; - error, float < double
+            f = Convert.ToSingle(d);
+            Console.WriteLine($"{d.GetType()} d = {d}");
+            Console.WriteLine($"{f.GetType()} f = Convert.ToSingle(d) = {d}");
+
+            //      Casting
+            Console.WriteLine("\nCasting:");
+            f = (float)d;
+            Console.WriteLine($"f = (float)d = {f}");
+
+            //      Problem of casting fail
+            d = 123456781234567890.999;
+            i = (int)d;
+            Console.WriteLine($"Casting overfit d = {d}\n   i = (int)d = {i}");
+
+            //      Types of Data in Expressions
+            Console.WriteLine("\nTypes of Data in Expressions");
+            int j = 2;
+            f = i/j;
+            Console.WriteLine($"{i.GetType()} i = {i}");
+            Console.WriteLine($"{j.GetType()} i = {j}");
+            Console.WriteLine($"{f.GetType()} f = i/j = {f}");
+            f = (float)i / (float)j;
+            Console.WriteLine($"{f.GetType()} f = (float)i / (float)j = {f}");
 
             Console.ReadKey();
         }
---
+++ 3 Conditions/Conditions/Conditions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for condition
+            Console.Write("Enter number 1-10: ");
+            int i = int.Parse(Console.ReadLine());
+            if (i < 3)
+            {
+                Console.WriteLine($"{i} < 3");
+            } else if (i >= 3 && i < 7)
+            {
+                Console.WriteLine($"3 <= {i} < 7");
+            } else if ( i == 9) {
+                Console.WriteLine("You entered 9");
+            } else
+            {
+                Console.WriteLine($"{i} >= 7");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
