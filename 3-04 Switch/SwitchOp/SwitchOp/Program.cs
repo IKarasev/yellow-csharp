57ca0a558e3a36c6174e86b32df2ec010cd76c4b
Ilya Karasev <iakarsev@gmail.com>
2021-07-09 18:01:55 +0300

    complited switch

--- 3-04 Switch/SwitchOp/SwitchOp/Program.cs
+++ 3-04 Switch/SwitchOp/SwitchOp/Program.cs
@@ -10,8 +10,59 @@ namespace SwitchOp
     {
         static void Main(string[] args)
         {
-            int i = ReadeIntVal();
+            int winType = ReadeIntVal("Enter windows type", 1, 3);
+
+            // Switch operation
+            switch (winType) {
+                case 1:
+                    handleCasement();
+                    break;
+                case 2:
+                    handleStandard();
+                    break;
+                case 3:
+                    handlePatio();
+                    break;
+                default:
+                    Console.WriteLine("Wrong type number");
+                    break;
+            }
+
+
+            // multiple cases in switch
+            string command = "casement";
+            switch (command) {
+                case "c":
+                case "casement":
+                    handleCasement();
+                    break;
+                case "s":
+                case "standard":
+                    handleStandard();
+                    break;
+                case "p":
+                case "patio":
+                    handlePatio();
+                    break;
+                default:
+                    Console.WriteLine("Wrong type");
+                    break;
+            }
+
             Console.ReadKey();
+        }
+
+        // methods to deal with different window types
+        static void handleCasement() {
+            Console.WriteLine("Handle Casement");
+        }
+
+        static void handleStandard() {
+            Console.WriteLine("Handle Standard");
+        }
+
+        static void handlePatio() {
+            Console.WriteLine("Handle patio");
         }
 
         static int ReadeIntVal(string intro = "Enter number", int min = 0, int max = 100)
