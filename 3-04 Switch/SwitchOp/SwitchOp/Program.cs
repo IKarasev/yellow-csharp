b4c0e6ffb4783ea5694e86ff673bae6aa04dd285
Ilya Karasev <iakarsev@gmail.com>
2021-07-09 17:49:11 +0300

    Changed folder naming. Started Switch. Added exception in ReadInt method.

---
+++ 3-04 Switch/SwitchOp/SwitchOp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = ReadeIntVal();
+            Console.ReadKey();
+        }
+
+        static int ReadeIntVal(string intro = "Enter number", int min = 0, int max = 100)
+        {
+            int res = 0;
+            do
+            {
+                Console.Write($"\n{intro} in range [{min};{max}]: ");
+                try {
+                    res = int.Parse(Console.ReadLine());
+                } catch (Exception e)
+                {
+                    Console.WriteLine($"Smth got wrong, try again: {e.Message}");
+                    res = min - 1;
+                }
+            } while ((res < min) || (res > max));
+            return res;
+        }
+    }
+}
