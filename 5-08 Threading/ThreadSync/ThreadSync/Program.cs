c52772716d88396ebaccf2d7d288f0d7e435b46c
Ilya Karasev <iakarsev@gmail.com>
2021-07-26 20:08:56 +0300

    complited ?? threads

---
+++ 5-08 Threading/ThreadSync/ThreadSync/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadSync
+{
+    class Program
+    {
+        static int sum = 0;
+
+        // empty object to hold as a token
+        static object sync = new object();
+
+        static private void SumArray(int start, int end)
+        {
+            int i;
+            for (i = start; i< end; i++)
+            {
+                sum++;
+            }
+        }
+
+        // creating methods that split up sumArray
+        // remember: thread delegates accept methods with NO input and output
+        static void Sum1()
+        {
+            SumArray(0, 5000000);
+        }
+
+        static void Sum2()
+        {
+            SumArray(5000000, 9999999);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("count directly");
+            SumArray(0, 9999999);
+            Console.WriteLine($"    sum = {sum}");
+            sum = 0;
+
+            Console.WriteLine("Start counting via threads");
+
+            // Creating first thread
+            ThreadStart ts1 = new ThreadStart(Sum1);
+            Thread t1 = new Thread(ts1);
+            t1.Start();
+
+            // Creating second thread
+            ThreadStart ts2 = new ThreadStart(Sum2);
+            Thread t2 = new Thread(ts2);
+            t2.Start();
+
+            Console.WriteLine($"    sum = {sum}"); // doesn't work well. Need sync
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5-08 Threading/Threading/Threading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static private void busyLoop()
+        {
+            long count;
+            for (count=0; count < 1000000000L; count++)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // this is "delegate". it sets starting point of new thread
+            // it can refer to a method that does not return a value or accept any parameters
+            ThreadStart busyLoopMethod = new ThreadStart(busyLoop);
+
+            // creating thread val
+            Thread t1 = new Thread(busyLoopMethod); // thread still not running
+            t1.Start(); //starting thread
+            //busyLoop(); // it goes at same time
+
+            // it is possible to create many threads
+            int i;
+            for (i = 0; i < 20; i++)
+            {
+                Thread t2 = new Thread(busyLoopMethod);
+                t2.Start();
+            }
+        }
+    }
+}
